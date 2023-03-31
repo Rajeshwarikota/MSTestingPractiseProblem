404bd246842169427d7969c94022d2aeec2f783b
Rajeshwarikota <kotarajeswari003.com>
2023-03-31 23:14:39 +0530

    [Rajeshwari kota] Adding Englisg Competation using MS Testing

---
+++ MSTestingPractiseProblem/EnglishCompetion.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSTestingPractiseProblem
+{
+    public class EnglishCompetion
+    {
+        public string SwappingCompetion(string word1, string word2)
+        {
+            bool eligible = true;
+            for (int i = 0; i < word2.Length; i++)
+            {
+                bool found = false;
+                for (int j = 0; j < word1.Length; j++)
+                {
+                    if (word2[i] == word1[j])
+                    {
+                        word1 = word1.Remove(j, 1);
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found)
+                {
+                    eligible = false;
+                    break;
+                }
+            }
+
+            if (eligible)
+            {
+                return "YES,Batch is Eligible";
+            }
+            else
+            {
+                return "NO,Batch is not Eligible";
+            }
+        }
+    }
+}
--- MSTestingPractiseProblem/Program.cs
+++ MSTestingPractiseProblem/Program.cs
@@ -11,6 +11,19 @@ namespace MSTestingPractiseProblem
         static void Main(string[] args)
         {
             Console.WriteLine("....MSTesting Problems....");
+            Console.WriteLine("Enter the below option");
+            Console.WriteLine("\n1:English competation\n22:default");
+            int choose = Convert.ToInt32(Console.ReadLine());
+            switch (choose)
+            {
+                case 1:
+                    EnglishCompetion ec = new EnglishCompetion();
+                    string word1 = "part";
+                    string word2 = "trap";
+                    string result = ec.SwappingCompetion(word1, word2);
+                    Console.WriteLine(result);
+                    break;
+            }
             Console.ReadLine();
         }
     }
